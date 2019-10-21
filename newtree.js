0f9926abf1e638a064ba721c656830e7f4e4fb04
S031402112 <42939395+S031402112@users.noreply.github.com>
2019-10-21 10:20:50 +0800

    Add files via upload

---
+++ newtree.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2019/10/19.
+ */
+ var myFunction(){
+    var x=document.getElementById("textId").value ;
+
+}
+
+var myFunction2(){
+    document.getElementById("textId2").value = x;
+    document.getElementByName("stutree").value = tree;
+}
+
+object	object.style.backgroundImage="url(picone.jpg)"
